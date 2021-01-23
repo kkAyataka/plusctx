7ed9a034c3e20dd2d371082dd3f09095c143ed3d
kkAyataka <kk.ayataka@gmail.com>
2021-01-23 11:28:00 +0900

    Import initial code

---
+++ include/plusctx/plusctx.hpp
@@ -0,0 +1,94 @@
+// Copyright (C) 2020 kkAyataka
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef PLUSCTX_HPP__
+#define PLUSCTX_HPP__
+
+#include <string>
+
+namespace plusctx {
+namespace detail {
+
+constexpr bool is_match(const char * s1, const char * s2) {
+    for (std::size_t i = 0; s1[i] && s2[i]; ++i) {
+        if (s1[i] != s2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+constexpr const char * shorten_file_name(const char * fileName) {
+#ifdef _MSC_VER
+    const char sep = '\\';
+    const char * src = "src\\";
+    const char * inc = "include\\";
+    const char * tes = "test\\";
+#else
+    const char sep = '/';
+    const char * src = "src/";
+    const char * inc = "include/";
+    const char * tes = "test/";
+#endif
+
+    for (std::size_t i = 0; fileName[i]; ++i) {
+        const auto c = fileName + i;
+        if (*c == sep) {
+            const auto c1 = c + 1;
+            if ((*c1 == src[0] && is_match(c1, src)) ||
+                (*c1 == inc[0] && is_match(c1, inc)) ||
+                (*c1 == tes[0] && is_match(c1, tes))) {
+                return c + 1;
+            }
+        }
+    }
+
+    return fileName;
+}
+
+} // namespace plusctx::detail
+
+struct Context {
+    Context(
+        const std::string name,
+        const std::string func_name,
+        const std::string rich_func_name,
+        const std::string file_name,
+        const std::size_t line_no
+    ) : name(name),
+        func_name(func_name),
+        rich_func_name(rich_func_name),
+        file_name(file_name),
+        line_no(std::to_string(line_no)) {
+    }
+
+    virtual ~Context() {
+    }
+
+    const std::string name;
+    const std::string func_name;
+    const std::string rich_func_name;
+    const std::string file_name;
+    const std::string line_no;
+};
+
+} // namespace plusctx
+
+#ifdef _MSC_VER
+#define PLUSCTX_RICHFUNCNAME __FUNCSIG__
+#else
+#define PLUSCTX_RICHFUNCNAME __PRETTY_FUNCTION__
+#endif
+
+#define PLUSCTX_CTX(name) plusctx::Context plusctx_ctx__( \
+    name, \
+    __func__, \
+    PLUSCTX_RICHFUNCNAME, \
+    plusctx::detail::shorten_file_name(__FILE__), \
+    __LINE__)
+
+#endif // PLUSCTX_HPP__
---
+++ test/unit_test/main.cpp
@@ -0,0 +1,9 @@
+#include "plusctx/plusctx.hpp"
+
+#include <iostream>
+
+int main() {
+    PLUSCTX_CTX("main");
+
+    return 0;
+}
